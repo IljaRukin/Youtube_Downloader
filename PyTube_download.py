bec7fccb532dd8c5d4bd942cf750e460f416b6b0
Ilja Rukin <ilja.rukin@gmail.com>
2023-12-30 23:07:47 +0100

    gui done

--- PyTube_download.py
+++ PyTube_download.py
@@ -6,6 +6,7 @@ import pytube
 from moviepy.editor import AudioFileClip
 from pathvalidate import sanitize_filename
 import tkinter as tk
+import tkinter.filedialog
 import tkinter.ttk as ttk
 import traceback
 #print(traceback.format_exc())
@@ -232,7 +233,7 @@ class DownloadHandler():
 		return None
 
 	### download loop
-	def downloadFiles(self, newLinksFile, downloadOnlyAudio=True):
+	def downloadFiles(self, newLinksFile, downloadOnlyAudio=True, authenticate=False):
 		newLinksPath = os.path.join(self.toDownloadLinksFolder,newLinksFile)
 		failed = list()
 		try:
@@ -246,11 +247,17 @@ class DownloadHandler():
 						line = line.strip('\n')
 						try:
 							print('downloading: ',line)
-							vid = pytube.YouTube( "http://youtube.com/watch?v="+line )
-							if downloadOnlyAudio:
-								stream = vid.streams.get_audio_only() #also possible method: .filterName(only_audio=True)[0]
+							if authenticate:
+								vid = pytube.YouTube("http://youtube.com/watch?v="+line,
+														 use_oauth=True, allow_oauth_cache=True)
 							else:
-								stream = vid.streams.filter(progressive=True).get_highest_resolution() #legacy: audio & video combined -> lower quality
+								vid = pytube.YouTube("http://youtube.com/watch?v="+line)
+							if downloadOnlyAudio:
+								#stream = vid.streams.filter(only_audio=True).order_by('resolution').desc().first()
+								stream = vid.streams.filter(only_audio=True).get_audio_only()
+							else:
+								#stream = vid.streams.filter(progressive=True).order_by('abr').desc().first()
+								stream = vid.streams.filter(progressive=True).get_highest_resolution()
 								#for highest video quality download video and audio separate [.filterName(adaptive=True)] and recombine (e.g. with ffmpeg)
 								'''
 								import ffmpeg
@@ -299,50 +306,251 @@ class DownloadHandler():
 		else:
 			print('no .pm4 files left !')
 
-
-def doStuff():
-	return None
-
 ### GUI
 
-#empty window
-root = tk()
+class Gui(tk.Tk):
+	def __init__(self):
+		self.session = DownloadHandler()
+		
+		super().__init__()
+		self.title("Youtube Downloader")
 
-#download all links -> loop over all toDownloadFiles
-#-----
-#list downloaded playlists/channels
-#extract new links from playlists/channels
-#file -> remove downloaded links
+		log_frame = tk.Frame(self)#, width=300, height=200
+		log_frame.pack(side = tk.TOP, padx='5', pady='5')
+		log_frame.rowconfigure(0, weight=1)
+		log_frame.columnconfigure(0, weight=1)
+		
+		#log output
+		textbox = tk.Text(log_frame, height=6)
+		textbox.pack(side = tk.TOP)
+		textbox.insert('1.0', "...")
+		textbox.config(state='disabled')
+		scrollb = tk.Scrollbar(log_frame, command=textbox.yview)
+		textbox.config(yscrollcommand=scrollb.set)
+		
+		useAuthentication_frame = tk.Frame(self)
+		useAuthentication_frame.pack(side = tk.TOP, padx='5', pady='5')
 
-#content: frame2 (login)
-frame2= tk.Frame(root, width=400, height=200)
-#frame2.pack_propagate(0)
-frame2.grid_propagate(0)
-frame2.pack()
-label5 = tk.Label(frame2, text='Name')
-label6 = tk.Label(frame2, text='Password')
-entry1 = tk.Entry(frame2)
-entry2 = tk.Entry(frame2)
-submitb = tk.Button(frame2, text='submit', command=print("test"))
+		tk.Label(useAuthentication_frame, text="use authentication when downloading ?").pack(side = tk.TOP)
 
-label5.grid(row=0,column=0, sticky=tk.E) #NESW
-label6.grid(row=1,column=0, sticky=tk.E)
-entry1.grid(row=0,column=1)
-entry2.grid(row=1,column=1)
-submitb.grid(row=4,columnspan=2)
+		#radiobutton video/audio -> filename video/audio
+		self.useAuthentication = tk.IntVar()
+		tk.Radiobutton(useAuthentication_frame,
+					text="yes authenticate",
+					padx = 20,
+					variable=self.useAuthentication,
+					value=True,
+					command=self.hideMediaSelectionFrame).pack(side = tk.LEFT)
+		tk.Radiobutton(useAuthentication_frame,
+					text="no authentication",
+					padx = 20,
+					variable=self.useAuthentication,
+					value=False,
+					command=self.showMediaSelectionFrame).pack(side = tk.LEFT)
+		self.useAuthentication.set(False)
+		
+		processingType_frame = tk.Frame(self)
+		processingType_frame.pack(side = tk.TOP, padx='5', pady='5')
+
+		tk.Label(processingType_frame, text="link extraction or media download ?").pack(side = tk.TOP)
+
+		#radiobutton video/audio -> filename video/audio
+		self.extractOnly = tk.IntVar()
+		tk.Radiobutton(processingType_frame,
+					text="extract",
+					padx = 20,
+					variable=self.extractOnly,
+					value=True,
+					command=self.hideMediaSelectionFrame).pack(side = tk.LEFT)
+		tk.Radiobutton(processingType_frame,
+					text="download",
+					padx = 20,
+					variable=self.extractOnly,
+					value=False,
+					command=self.showMediaSelectionFrame).pack(side = tk.LEFT)
+		self.extractOnly.set(False)
+		
+		self.mediaSelection_frame = tk.Frame(self)
+		self.mediaSelection_frame.pack(side = tk.TOP, padx='5', pady='5')
+
+		tk.Label(self.mediaSelection_frame, text="for download select media output format:").pack(side = tk.TOP)
+
+		#radiobutton video/audio -> filename video/audio
+		self.audioOnly = tk.IntVar()
+		tk.Radiobutton(self.mediaSelection_frame,
+					text="audio",
+					padx = 20,
+					variable=self.audioOnly,
+					value=True).pack(side = tk.LEFT)
+		tk.Radiobutton(self.mediaSelection_frame,
+					text="video",
+					padx = 20,
+					variable=self.audioOnly,
+					value=False).pack(side = tk.LEFT)
+		self.audioOnly.set(True)
+		
+		#link	 -> extract/download
+		self.link_frame = tk.Frame(self)
+		self.link_frame.pack(side = tk.TOP, padx='5', pady='5')
+
+		self.link = tk.StringVar()
+		self.link_entry = tk.Entry(self.link_frame, textvariable = self.link,
+								 font=('calibre',10,'normal'), width = 50)
+		self.link_entry.pack(side = tk.LEFT, padx='5', pady='5')
+		self.link_button = tk.Button(self.link_frame, command=self.processLink,
+									 text='one Link', width = 14)
+		self.link_button.pack(side = tk.LEFT, padx='5', pady='5')
+
+		#playlist -> extract/download
+		self.playlist_frame = tk.Frame(self)
+		self.playlist_frame.pack(side = tk.TOP, padx='5', pady='5')
+
+		self.playlist = tk.StringVar()
+		self.link_entry = tk.Entry(self.playlist_frame, textvariable = self.playlist,
+								 font=('calibre',10,'normal'), width = 50)
+		self.link_entry.pack(side = tk.LEFT, padx='5', pady='5')
+		self.playlist_button = tk.Button(self.playlist_frame, command=self.processPlaylist,
+									 text='full Playlist', width = 14)
+		self.playlist_button.pack(side = tk.LEFT, padx='5', pady='5')
+		
+		#channel	-> extract/download
+		self.channel_frame = tk.Frame(self)
+		self.channel_frame.pack(side = tk.TOP, padx='5', pady='5')
+
+		self.channel = tk.StringVar()
+		self.channel_entry = tk.Entry(self.channel_frame, textvariable = self.channel,
+								 font=('calibre',10,'normal'), width = 50)
+		self.channel_entry.pack(side = tk.LEFT, padx='5', pady='5')
+		self.channel_button = tk.Button(self.channel_frame, command=self.processChannel,
+									text='Channel uploads', width = 14)
+		self.channel_button.pack(side = tk.LEFT, padx='5', pady='5')
+		
+		#----------
+		
+		ttk.Separator(self, orient='horizontal').pack()
+		
+		#extracted Linklist	 -> download
+		self.file_frame = tk.Frame(self)
+		self.file_frame.pack(side = tk.TOP, padx='5', pady='5')
+
+		self.file = tk.StringVar()
+		self.file_entry = tk.Entry(self.file_frame, textvariable = self.file,
+								 font=('calibre',10,'normal'), width = 50)
+		self.file_entry.pack(side = tk.LEFT, padx='5', pady='5')
+		self.file_entry.bind("<1>", self.browseFiles)
+		self.file_button = tk.Button(self.file_frame, command=self.processFile,
+									 text='download specific extracted', width = 24)
+		self.file_button.pack(side = tk.LEFT, padx='5', pady='5')
+
+		#all Linklists	-> extract/download
+		self.allFiles_frame = tk.Frame(self)
+		self.allFiles_frame.pack(side = tk.TOP, padx='5', pady='5')
+
+		self.allFiles_button = tk.Button(self.allFiles_frame, command=self.processAllFiles,
+									 text='download All extracted', width = 24)
+		self.allFiles_button.pack(side = tk.LEFT, padx='5', pady='5')
+
+		#----------
+
+		self.link.set("https://www.youtube.com/watch?v=dOVvmUqmRCk")
+		self.playlist.set("https://www.youtube.com/playlist?list=UUwIgPuUJXuf2nY-nKsEvLOg")
+		self.channel.set("https://www.youtube.com/channel/UCwIgPuUJXuf2nY-nKsEvLOg")
+		self.file.set("C:/Users/User/Downloads/Youtube_Downloader/toDownloadLinks/test.txt")
+		
+	def hideMediaSelectionFrame(self):
+		self.mediaSelection_frame.pack_forget()
+			
+	def showMediaSelectionFrame(self):
+		self.mediaSelection_frame.pack(side = tk.TOP, padx='5', pady='5',
+							before=self.file_frame)
+		
+	def browseFiles(self, event):
+		filename = tk.filedialog.askopenfilename(initialdir = "./toDownloadLinks",
+					  title = "Select Link file",
+					  filetypes = (("Text files",
+							"*.txt*"),
+							   ("all files",
+							"*.*")))
+		self.file.set(filename)
+		return None
+	
+	def processFile(self):
+		'''
+		extractedLinks = session.collectLinks(playlistUrl)
+		session.saveLinks(extractedLinks, newLinksFile)
+		session.compareOldLinks(newLinksFile)
+		audioOnly = self.audioOnly.get()
+		authenticate = self.useAuthentication.get()
+		session.downloadFiles(newLinksFile, audioOnly, authenticate)
+		'''
+		
+	def processLink(self):
+		if self.extractOnly.get():
+			print("extracting link: "+str(self.file.get()))
+		else:
+			print("downloading link: "+str(self.file.get()))
+		extractedLinks = session.collectLinks(playlistUrl)
+		session.saveLinks(extractedLinks, newLinksFile)
+		session.compareOldLinks(newLinksFile)
+		audioOnly = self.audioOnly.get()
+		authenticate = self.useAuthentication.get()
+		session.downloadFiles(newLinksFile, audioOnly, authenticate)
+		
+	def processPlaylist(self):
+		if self.extractOnly.get():
+			print("extracting playlist: "+str(self.playlist.get()))
+		else:
+			print("downloading playlist: "+str(self.playlist.get()))
+			#self.audioOnly.get()
+		
+	def processChannel(self):
+		if self.extractOnly.get():
+			print("extracting channel: "+str(self.channel.get()))
+		else:
+			print("downloading channel: "+str(self.channel.get()))
+			#self.audioOnly.get()
+		
+	def processFile(self):
+		print("download selected extracted Links")
+		
+	def processAllFiles(self):
+		print("download all extracted Links")
+		
+	def promptSelection(self):
+		print("running")
+		title = "my title"
+		message = "my message"
+		windowPopUp(title, message)
+
+class windowPopUp(tk.Toplevel):
+	
+	def __init__(self, title, message):
+		tk.Toplevel.__init__(self)
+		self.title(title)
+		self.geometry('350x75')
+		self.minsize(350, 75)
+		self.maxsize(425, 250)
+		self.rowconfigure(0, weight=0)
+		self.rowconfigure(1, weight=1)
+		self.columnconfigure(0, weight=1)
+
+		button_frame = tk.Frame(self)
+		button_frame.grid(row=0, column=0, sticky='nsew')
+		button_frame.columnconfigure(0, weight=1)
+		button_frame.columnconfigure(1, weight=1)
+
+		ttk.Label(button_frame, text=message).grid(row=0, column=0, columnspan=2, pady=(7, 7))
+		ttk.Button(button_frame, text='OK', command=self.destroy).grid(row=1, column=0, sticky='e')
+		ttk.Button(button_frame, text='Details', command=self.selectDuplicates).grid(row=1, column=1, sticky='w')
+	
+	@staticmethod
+	def selectDuplicates():
+		print("test")
+
+if __name__ == '__main__':
+	App = Gui().mainloop()
 
 '''
-response = Label(frame2, text=' ')
-response.grid(row=5,columnspan=2)
-
-if answer=='yes':
-	print('you said yes')
-else:
-	print('you said no')
-
-#keep window open
-root.mainloop()
-
 url = "https://www.youtube.com/@wavemusic"
 "browseId":"UCbuK8xxu2P_sqoMnDsoBrrg"
 "https://www.youtube.com/playlist?list=UU"+"buK8xxu2P_sqoMnDsoBrrg"
@@ -351,18 +559,3 @@ url = "https://www.youtube.com/@AirwaveMusicTV/videos"
 "browseId":"UCwIgPuUJXuf2nY-nKsEvLOg"
 "https://www.youtube.com/playlist?list=UU"+"wIgPuUJXuf2nY-nKsEvLOg"
 '''
-
-
-'''
-#if __name__ == "__main__":
-session = DownloadHandler()
-newLinksFile = "test.txt"#input("path to new links file")
-playlistUrl = "https://www.youtube.com/playlist?list=PLndEULTswG5aTuZ4gGTCZ-PWx3JUCFjyk"#input("url to youtube playlist")#
-
-extractedLinks = session.collectLinks(playlistUrl)
-session.saveLinks(extractedLinks, newLinksFile)
-session.compareOldLinks(newLinksFile)
-audioOnly = True
-session.downloadFiles(newLinksFile,audioOnly)
-'''
-
